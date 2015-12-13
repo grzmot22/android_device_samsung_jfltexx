3beea71f7355ee4aea947f77b767067c63a39a80
grzmot22 <grzmot22@users.noreply.github.com>
2015-12-13 21:37:44 +0100

    Add JDC stuff

--- cm.mk
+++ cm.mk
@@ -6,6 +6,9 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Inherit some common JDC stuff.
+$(call inherit-product, vendor/jdc/config/common.mk)
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltexx \
   TARGET_DEVICE=jfltexx \
